837959e08c985f23998a3bb2adaaf2b4bb41e6ad
Jesse <196892153+Hjesse25@users.noreply.github.com>
2026-02-03 15:45:38 -0800

    section 7 complete

---
+++ Working with Dates/DateTime/Example/Program.cs
@@ -0,0 +1,15 @@
+﻿var dateTime = new DateTime(2026, 2, 2);
+var now = DateTime.Now;
+var today = DateTime.Today;
+
+// Console.WriteLine("Hour: " + now.Hour);
+// Console.WriteLine("Minute: " + now.Minute);
+
+var tomorrow = now.AddDays(1);
+var yesterday = now.AddDays(-1);
+
+Console.WriteLine(now.ToLongDateString());
+Console.WriteLine(now.ToShortDateString());
+Console.WriteLine(now.ToLongTimeString());
+Console.WriteLine(now.ToShortTimeString());
+Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
