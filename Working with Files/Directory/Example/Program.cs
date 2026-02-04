0e8ac102db1dcb1f308ac11c1ca88d7fcc57501f
Jesse <196892153+Hjesse25@users.noreply.github.com>
2026-02-03 16:11:56 -0800

    section 9 complete

---
+++ Working with Files/Directory/Example/Program.cs
@@ -0,0 +1,16 @@
+﻿Directory.CreateDirectory(@"/home/shadowk/folder1");
+
+var files = Directory.GetFiles(@"/home/shadowk/Code/CSharpFundamentals/", "*.slnx", SearchOption.AllDirectories);
+foreach (var file in files)
+    Console.WriteLine(file);
+
+var directories = Directory.GetDirectories(@"/home/shadowk/Code/CSharpFundamentals/", "*.*", 
+    SearchOption.AllDirectories);
+foreach (var directory in directories)
+    Console.WriteLine(directory);
+
+Directory.Exists("...");
+
+var directoryInfo = new DirectoryInfo("...");
+directoryInfo.GetFiles();
+directoryInfo.GetDirectories();
---
+++ Working with Files/File/Example/Program.cs
@@ -0,0 +1,17 @@
+﻿var path = @"[FilePath]";
+
+File.Copy("", "", true);
+File.Delete(path);
+if (File.Exists(path))
+{
+    //
+}
+var content = File.ReadAllText(path);
+
+var fileInfo = new FileInfo(path);
+fileInfo.CopyTo("...");
+fileInfo.Delete();
+if (fileInfo.Exists)
+{
+    //
+}
